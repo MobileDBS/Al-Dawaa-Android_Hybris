40680b90686919cd19eb4b857a251cc70753c87b
sohir <sohirhashem@gmail.com>
2022-09-11 16:43:33 +0200

    first commit

--- app/src/main/java/com/kr/aldawaa/MainActivity.kt
+++ app/src/main/java/com/kr/aldawaa/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.kr.aldawaa.ui.theme.AlDawaaHybrisTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
