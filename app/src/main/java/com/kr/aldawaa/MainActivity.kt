28467e05669db0d59a729fe3c7675e1ca213ff3f
PC <shimaasamir54@gmail.com>
2022-09-25 14:29:19 +0200

    BaseUrl

--- app/src/main/java/com/kr/aldawaa/MainActivity.kt
+++ app/src/main/java/com/kr/aldawaa/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
