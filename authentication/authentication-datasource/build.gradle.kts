228e425f714d69d3b5c6bec4a63fc916001c65a9
PC <shimaasamir54@gmail.com>
2022-09-27 08:58:34 +0200

    BaseUrl

--- authentication/authentication-datasource/build.gradle.kts
+++ authentication/authentication-datasource/build.gradle.kts
@@ -13,8 +13,4 @@ dependencies{
     "implementation"(project(Modules.authenticationDomain))
 
     "implementation"(Kotlinx.coroutinesCore)
-
-   // "implementation"(Hilt.android)
-   // "kapt"(Hilt.compiler)
-
 }
--- authentication/authentication-datasource/src/main/java/com/kr/authentication_datasource/network/AuthenticationRepoImp.kt
+++ authentication/authentication-datasource/src/main/java/com/kr/authentication_datasource/network/AuthenticationRepoImp.kt
@@ -1,47 +1,9 @@
 package com.kr.authentication_datasource.network
 
-import com.kr.authentication_datasource.network.dto.LoginResponse
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
-class AuthenticationRepoImp  {
-
-/*    suspend fun getLoginDataa(
-        apiInterface: ApiInterface,
-    ):
-            LoginResponse {
-        return apiInterface.getDirections(
-                "driving",
-                "less_driving",
-                "AIzaSyCh3G_4p1rllyDCdJ53HoxdYa5x_q_Gytw",
-                startFromDb,
-                endFromServer
-            )
-
-    }*/
-
-/*    suspend fun getLoginData(
-        startFromDb: String,
-        endFromServer: String
-    ): Flow<LoginResponse> {
-        return flow {
-            val result = apiServicesGoogleDirections.getDirections(
-                "driving",
-                "less_driving",
-                "AIzaSyCh3G_4p1rllyDCdJ53HoxdYa5x_q_Gytw",
-                startFromDb,
-                endFromServer
-            ).await()
-            emit(result)
-        }.flowOn(Dispatchers.IO)
-    }*/
-}
-/*
-@Inject constructor(
+class AuthenticationRepoImp  @Inject constructor(
     private  val api: CoinPaprikaApi
-)
-{
-
+) : CoinRepository{
     override suspend fun getCoins(): List<CoinDto> {
         return  api.getCoins()
     }
@@ -49,4 +11,3 @@ class AuthenticationRepoImp  {
     override suspend fun getCoinById(coinId: String): CoinDetailDto {
         return  api.getCoinById(coinId)
     }
-}*/
