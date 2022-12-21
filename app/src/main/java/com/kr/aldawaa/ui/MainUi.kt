7e8a7cb04100cc02955cf1059221d8b456773131
Abeer5555 <abeere5734@gmail.com>
2022-12-22 00:37:42 +0200

    new_home_product_list

--- app/src/main/java/com/kr/aldawaa/ui/MainUi.kt
+++ app/src/main/java/com/kr/aldawaa/ui/MainUi.kt
@@ -61,7 +61,7 @@ fun BottomNavGraph(navController: NavHostController) {
 
         composable("Category_Items", content = { CategoryDetailsScreen(navController = navController) })
 
-        composable("Product_List", content = { ProductListScreen(navController=navController, {}) })
+        composable("Product_List", content = { ProductListScreen(navController=navController) })
         composable("Camera_Screen?showvalu={showvalu}", arguments = listOf(navArgument(name ="showvalu" ){
             type = NavType.BoolType
         }), content = {
--- app/src/main/java/com/kr/aldawaa/ui/NavigationController.kt
+++ app/src/main/java/com/kr/aldawaa/ui/NavigationController.kt
@@ -39,7 +39,7 @@ fun NavigationController() {
 
             composable("MainUi", content = { MainScreen() })
 
-            composable("Product_List", content = { ProductListScreen(navController=navController) {} })
+            composable("Product_List", content = { ProductListScreen(navController=navController) })
 
             /* composable("MainUi", content = { MainUi(navController = navController) })
 
